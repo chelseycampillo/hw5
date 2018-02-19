3dadb27783f642109dc64fecd02fc8d3eb794fd9
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-02-18 21:40:24 -0800

    Update smoke-arrays.js

--- smoke-arrays.js
+++ smoke-arrays.js
@@ -1,14 +1,17 @@
-var x = 200;
+var x = [];
 var y = [];
 var ySpeed = [];
+var xSpeed = [];
 var r = 0;
 
 function setup() {
   createCanvas(400, 400);
 	
-	for (var i = 0; i < 100; i ++) {
+	for(var i = 0; i < 100; i++){
+		x[i] = random(190,220);
 		y[i] = random(0,height);
-		ySpeed[i] = random(0, 10);
+		ySpeed[i] = random(0,10);
+		xSpeed[i] = random(-5,5);
 	}
 }
   
@@ -18,25 +21,28 @@ function draw() {
 
   // draw smokestack
   fill(255);
-  rect(195, 300, 30, 100);
+  rect(195, height, 30, -100);
 
   // save graphics state
-	for (var i = 0; i < 100; i ++) {
+	for (var i = 0; i < 100; i++) {
   	push();
-  	rect(x, y[i], 20, 20);
-  	pop();
+		fill(y[i]);
+		translate(x[i], y[i]);
+  	rotate(r);
+  	rect(-10, -10, 20, 20);
+		pop();
 	}
   
   // up 3 pixels
-	for (var i = 0; i < 100; i ++) {
-  	y[i] = y[i] + ySpeed[i];
+	for (var i = 0; i < 100; i++) {
+  	y[i] -= ySpeed[i];
 	}
   
   // rotate 0.05 radians ~= 2.8 degrees per frame
   r += 0.05
-  
-  // if reach past the top a bunch
-  if (y < -150) {
-    y = 290;
-  }
+	
+	//move horizontally
+	for (var i = 0; i < 100; i++) {
+		x[i] += xSpeed[i];
+	}
 }
